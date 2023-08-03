f62beaf8c50635467ddc3e4e020a7422afce0a2c
Can <cannkaynarr@outlook.com>
2023-08-03 18:37:23 +0300

    Initial Commit

---
+++ AtYarisi2/AtYarisi2App.swift
@@ -0,0 +1,17 @@
+//
+//  AtYarisi2App.swift
+//  AtYarisi2
+//
+//  Created by Can on 3.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AtYarisi2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
